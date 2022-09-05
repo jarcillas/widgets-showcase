40fd6faf26556115c5d8b3f9fdb15d51ae04c8aa
Jesreal Arcillas <jesrealarcillas@gmail.com>
2022-09-05 14:06:19 +0800

    add feature that shows list of wikipedia entries

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,68 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [results, setResults] = useState([]);
 
   const handleChange = (e) => {
     setSearchTerm(e.target.value);
   };
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          srsearch: searchTerm,
+          format: 'json',
+        },
+      });
+      setResults(data.query.search);
+    };
+
+    const timeoutId = setTimeout(() => {
+      if (searchTerm) {
+        search();
+      } else {
+        setResults([]);
+      }
+    }, 500);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [searchTerm]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className="ui button"
+            target="_blank"
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div className="ui container">
       <div className="ui segment">
         <div className="ui form">
           <div className="field">
-            <label htmlFor="">Enter Search Term</label>
+            <label>Search Wikipedia:</label>
             <input
               type="text"
               className="input"
@@ -22,6 +71,7 @@ const Search = () => {
             />
           </div>
         </div>
+        <div className="ui celled list">{renderedResults}</div>
       </div>
     </div>
   );
